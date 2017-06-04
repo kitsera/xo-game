fc53c6bd7a76b00bbf51ddd92816ccd55afff911
kitsera <sashakitsera16358@gmail.com>
2017-06-04 05:50:04 +0300

    first

--- Client.py
+++ Client.py
@@ -1,11 +1,21 @@
 # send socket data in field like '110011100'
 
 from socket import socket
-
+import time
 client_socket = socket()
 client_socket.connect(('192.168.1.103', 53211))
-client_socket.send(b'110011100')
-echo = client_socket.recv(9)
-client_socket.close()
-print('Recevied: ', echo)
-print(type(echo))
+game_state = client_socket.recv(1)
+if game_state == b'f' or game_state == b's':
+    player_state = input("Type 'r' when you are ready: ")
+    client_socket.send(bytes(player_state))
+    while True:
+        if game_state == b'f':
+            client_socket.send(b'first player move')
+            move = client_socket.recv(1024)
+        else:
+            move = client_socket.recv(1024)
+            client_socket.send(b'second player move')
+        time.sleep(3)
+        if not move:
+            break
+    client_socket.close()
--- server.py
+++ server.py
@@ -4,17 +4,43 @@
 # 011 game field
 
 from socket import socket
+import time
 
 server_socket = socket()
 server_socket.bind(('192.168.1.103', 53211))
 server_socket.listen(2)
 
-client_socket, client_addr = server_socket.accept()
 
 while True:
-    field = client_socket.recv(9)
-    print("Received field: ", field)
-    if not field:
-        break
-    client_socket.send(field)
+    # Connection cycle
+    client_first_sock, client_first_addr = server_socket.accept()
+    print('Connected by :', client_first_addr)
+    client_second_sock, client_second_addr = server_socket.accept()
+    print('Connected by :', client_second_addr)
 
+    print("Players are connected!")
+    client_first_sock.send(b'f')
+    client_second_sock.send(b's')
+
+    while True:
+        # Waiting for players ready
+        first_ready = client_first_sock.recv(1)
+        second_ready = client_second_sock.recv(1)
+        if first_ready == b'r' and second_ready == b'r':
+            print('Players are ready! ')
+            break
+
+    while True:
+        # Game cycle
+        first_field = client_first_sock.recv(1024)
+        client_second_sock.send(first_field)
+        time.sleep(3)
+        second_field = client_second_sock.recv(1024)
+        client_first_sock.send(second_field)
+        time.sleep(3)
+
+        if not first_field or not second_field:
+            break
+
+    client_first_sock.close()
+    client_second_sock.close()
